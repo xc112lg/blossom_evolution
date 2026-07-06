016e1533a8a4afd0904a6ed17c596b26f76dc79d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-06 10:50:05 +0800

    Update build4.sh

--- build4.sh
+++ build4.sh
@@ -40,9 +40,9 @@ sed -i '$a -include vendor/evolution-priv/keys/keys.mk' device/xiaomi/blossom/li
 #sed -i '/<item>com.android.nfc<\/item>/d' frameworks/base/core/res/res/values/policy_exempt_apps.xml
 #cat frameworks/base/core/res/res/values/policy_exempt_apps.xml
 
-m installclean
 lunch lineage_blossom-bp4a-user
 
+m installclean
 #m clean #once
 m evolution
 
